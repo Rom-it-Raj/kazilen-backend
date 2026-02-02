9f727e3dedac6118c5826f8a94d386ef39dd81c0
Rom <romitrajbind0@gmail.com>
2026-02-02 20:18:19 +0530

    !Automated message! Did work at Mon Feb  2 08:18:19 PM IST 2026

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -146,8 +146,8 @@ def db_check(request):
 class unporc_profile(Schema):
     user_id: str
 
-@api.post("/get_user_profile", response=CustomerSchema)
-def unporc_get_profile(request, data: unporc_profile):
-    user_id = data.user_id
-    user = get_object_or_404(Customer, id= user_id)
+@api.post("/get_user_profile")
+def unporc_get_profile(request, unporc_profile):
+    user_id = request.user_id
+    user = get_object_or_404(Customer, userID= user_id)
     return user
