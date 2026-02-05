b721c2b80d5fbee63a3279cb042a49d52c7de0e0
Rom <romitrajbind0@gmail.com>
2026-02-05 18:47:32 +0530

    !Automated message! Did work at Thu Feb  5 06:47:32 PM IST 2026

--- djangoproj/djangoapp/api.py
+++ djangoproj/djangoapp/api.py
@@ -4,7 +4,15 @@ from typing import List, Optional
 from ninja import FilterSchema, NinjaAPI, Query, Schema
 from django.shortcuts import get_object_or_404
 from .models import Customer, Worker, History
-from .schemas import CustomerSchema, WorkerSchema, HistorySchema, SendOTPSchema, VerifyOTPSchema,CreateAccountSchema, CreateWorkerSchema
+from .schemas import (
+    CustomerSchema,
+    WorkerSchema,
+    HistorySchema,
+    SendOTPSchema,
+    VerifyOTPSchema,
+    CreateAccountSchema,
+    CreateWorkerSchema,
+)
 import hashlib
 from .utils.otp_generator import otp_gen
 from .utils.send_otp import sendOTP_SMS, sendOTP_WHATSAPP
@@ -16,22 +24,24 @@ import secrets
 from .auth import CustomAuth
 from django.db import connections
 from django.db.utils import OperationalError
-db_conn =connections["default"]  #will change once we migrate to neon
+
+db_conn = connections["default"]  # will change once we migrate to neon
 
 load_dotenv()
 api = NinjaAPI()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 redis_client = Redis(
-        host = os.getenv("REDIS_URL"), 
-        port = int(os.getenv("REDIS_PORT")),
-        password=os.getenv("REDIS_PASSWORD"),
-        decode_responses=True
-        )
+    host=os.getenv("REDIS_URL"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True,
+)
 
 # class workerFilter(FilterSchema):
 #     category: Optional[List[str]]
 #     subcategory: Optional[List[str]]
+
 
 @api.get("/worker", response=List[WorkerSchema])
 def getAllWorker(request):
@@ -52,17 +62,19 @@ def getFilterWorker(
     workers = Worker.objects.filter(filters)
     return workers
 
+
 @api.post("/send-otp")
 def send_otp(request, payload: SendOTPSchema):
-   phone = payload.phone
-   otp = otp_gen()
-   logger.info(f"OTP: {otp}")
-   hashed = hashlib.sha256(otp.encode()).hexdigest()
-   logger.info(f"Hashed: {hashed}")
-   redis_client.setex(f"otp:{phone}", 600, hashed)
-   logger.info("STORED IN REDIS")
-   sendOTP_SMS(otp=otp, recpient=phone)
-   return {"status": True, "message": "OTP Sent successfully"}
+    phone = payload.phone
+    otp = otp_gen()
+    logger.info(f"OTP: {otp}")
+    hashed = hashlib.sha256(otp.encode()).hexdigest()
+    logger.info(f"Hashed: {hashed}")
+    redis_client.setex(f"otp:{phone}", 600, hashed)
+    logger.info("STORED IN REDIS")
+    sendOTP_SMS(otp=otp, recpient=phone)
+    return {"status": True, "message": "OTP Sent successfully"}
+
 
 @api.post("/verify-otp")
 def verify_otp(request, payload: VerifyOTPSchema):
@@ -71,7 +83,7 @@ def verify_otp(request, payload: VerifyOTPSchema):
     if not stored:
         return {"success": False, "error": "OTP expired or invalid"}
     input_hash = hashlib.sha256(payload.otp.encode()).hexdigest()
-    if input_hash != stored :
+    if input_hash != stored:
         return {"success": False, "error": "Invalid OTP entered"}
     session_token = secrets.token_urlsafe(32)
     logger.info(f"SESSION_TOKEN: {session_token}")
@@ -84,17 +96,22 @@ def protected_check(request):
     phone = request.auth
     if not phone:
         return {"error": "User does not exist", "status": False}
-    return {"message" : f"Your phone number = {phone}"}
+    return {"message": f"Your phone number = {phone}"}
 
 
 class check_phoneNo(Schema):
     phone: str
 
-@api.post('/check')
+
+@api.post("/check", response={200: CustomerSchema, 404: dict})
 def unprotected_check(request, data: check_phoneNo):
-    valid_phone = '+91'+data.phone
-    exists = Customer.objects.filter(phoneNo=valid_phone)
-    return {"exists": exists.exists()}
+    valid_phone = "+91" + data.phone
+    exists = Customer.objects.filter(phoneNo=valid_phone).first()
+    if exists:
+        return 200, exists
+    else:
+        return 404, {"messg": "yo no bud"}
+
 
 @api.get("/get-profile", auth=CustomAuth(), response=CustomerSchema)
 def get_profile(request):
@@ -103,6 +120,7 @@ def get_profile(request):
         return {"error": "User does not exist", "status": False}
     details = get_object_or_404(Customer, phoneNo=phone)
     return details
+
 
 @api.get("/get-history", auth=CustomAuth(), response=List[HistorySchema])
 def get_history(request):
@@ -113,10 +131,12 @@ def get_history(request):
     details = History.objects.filter(customer=customer).order_by("-timestmp")
     return details
 
+
 @api.post("/create-account")
-def create_account(request, payload:CreateAccountSchema):
+def create_account(request, payload: CreateAccountSchema):
     customer = Customer.objects.create(**payload.dict())
     return {"message": "User created successfully", "name": customer.name}
+
 
 @api.post("/create-worker")
 def create_worker(request, payload:CreateWorkerSchema):
@@ -133,6 +153,7 @@ def create_worker(request, payload:CreateWorkerSchema):
             )
     return {"message": f"Hello, {worker.name}", "status": True}
 
+
 @api.get("/db_health")
 def db_check(request):
     try:
@@ -140,11 +161,11 @@ def db_check(request):
             cursor.execute("SELECT 1")
             return {"status": "DB is up"}
     except OperationalError as e:
-        print(f"DB ERROR: {e}")  #testing purposes only
-        return {"status" : "DB is down"}
+        print(f"DB ERROR: {e}")  # testing purposes only
+        return {"status": "DB is down"}
 
 
-#kjkjdhkjshd
+# kjkjdhkjshd
 class unporc_profile(Schema):
     user_id: str
 
@@ -152,4 +173,9 @@ class unporc_profile(Schema):
 def unporc_get_profile(request, unporc_profile):
     user_id = request.user_id
     user = get_object_or_404(Customer, userID= user_id)
+
+@api.post("/get_user_profile", response=CustomerSchema)
+def unporc_get_profile(request, data: unporc_profile):
+    user_id = data.user_id
+    user = get_object_or_404(Customer, id=user_id)
     return user
