a061f13d06f02d419a3de288118d27df3cf2a2be
Rom <romitrajbind0@gmail.com>
2026-03-05 17:08:08 +0530

    !Automated message! Did work at Thu Mar  5 05:08:08 PM IST 2026

--- djangoproj/djangoapp/admin.py
+++ djangoproj/djangoapp/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import Customer, Worker, History
 admin.site.register(Customer)
 admin.site.register(History)
-
-@admin.register(Worker)
-class WorkerAdmin(admin.ModelAdmin):
-    readonly_fields=('id',)
-    fields = ('id', 'name', 'address', 'phoneNo', 'imageURL', 'category', 'subcategory', 'rating', 'price', 'description')
+admin.site.register(Worker)
